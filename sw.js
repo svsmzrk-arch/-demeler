103a6942d0945802c890e4506c8d3c7813d6995a
svsmzrk-arch <svsmzrk@gmail.com>
2026-06-21 13:48:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+var CACHE = 'odeme-v2';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(['./index.html', './manifest.json']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(ks) {
+      return Promise.all(ks.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(res) {
+        var clone = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(e.request, clone); });
+        return res;
+      }).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
